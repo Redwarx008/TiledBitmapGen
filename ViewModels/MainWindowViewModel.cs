ffa63fe40f79b68dfc9c2558cf5e71c773bf652d
Redwarx008 <redwarx008@outlook.com>
2023-09-14 18:14:24 +0800

    complete gui

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,64 @@
-﻿namespace TiledBitmapGen.ViewModels
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace TiledBitmapGen.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        [DllImport("TildBitMapLib.dll")]
+        private static extern int Add(int a, int b);
+
+        #region property
+        private string _filePath = string.Empty;
+        public string FilePath
+        {
+            get => _filePath;
+            set => SetProperty(ref _filePath, value);
+        }
+
+        #endregion
+
+        #region Command
+        public RelayCommand OpenFileCommand { get; }
+
+        private async void OpenPngFile()
+        {
+            if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+    desktop.MainWindow?.StorageProvider is not { } provider)
+                throw new NullReferenceException("Missing StorageProvider instance.");
+
+            var files = await provider.OpenFilePickerAsync(new FilePickerOpenOptions()
+            {
+                Title = "Open Png File",
+                AllowMultiple = false,
+                FileTypeFilter = new FilePickerFileType[] { FilePickerFileTypes.ImagePng },
+            });
+
+            if (files != null)
+            {
+                FilePath = files[0].Path.AbsolutePath.ToString();
+            }
+        }
+
+        public RelayCommand GenerateCommand { get; }
+
+        int res;
+        private void AddHandler()
+        {
+            res = Add(0, 8);
+        }
+
+        #endregion
+        public MainWindowViewModel() 
+        {
+            OpenFileCommand = new RelayCommand(OpenPngFile);
+            GenerateCommand = new RelayCommand(AddHandler);
+        }
     }
 }
