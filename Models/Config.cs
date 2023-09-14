8ce4a00d024ebe63e96f30800da9e5bbd890255f
Redwarx008 <50837890+Redwarx008@users.noreply.github.com>
2023-09-15 02:04:14 +0800

    add some logic

--- Models/Config.cs
+++ Models/Config.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TiledBitmapGen.Models
 {
-    public class Config
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Config
     {
+        public string FileName;
+        public int TileSize;
+        public bool IsHeightmap;
+        public float MinHeight;
+        public float MaxHeight;
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,22 +4,33 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using TiledBitmapGen.Models;
 
 namespace TiledBitmapGen.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        [DllImport("TildBitMapLib.dll")]
-        private static extern int Add(int a, int b);
+        [DllImport("TildBitMapLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool Create(Config config);
 
+        [DllImport("TildBitMapLib.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool GetImgInfo(string fileName, ref int nChannel, ref int bitDepth);
         #region property
         private string _filePath = string.Empty;
         public string FilePath
         {
             get => _filePath;
             set => SetProperty(ref _filePath, value);
+        }
+
+        private bool _isHeightmap = false;
+        public bool IsHeightmap
+        {
+            get => _isHeightmap;
+            set => SetProperty(ref _isHeightmap, value);
         }
 
         #endregion
@@ -42,23 +53,33 @@ namespace TiledBitmapGen.ViewModels
 
             if (files != null)
             {
-                FilePath = files[0].Path.AbsolutePath.ToString();
+                FilePath = files[0].Path.LocalPath;
+            }
+
+            int nChannel = 0;
+            int bitDepth = 0;
+            GetImgInfo(_filePath, ref nChannel, ref bitDepth);
+            if (nChannel == 1 && bitDepth == 16) 
+            {
+                IsHeightmap = true;
             }
         }
 
         public RelayCommand GenerateCommand { get; }
 
-        int res;
-        private void AddHandler()
+        private void Generate()
         {
-            res = Add(0, 8);
+            Config config = new Config()
+            {
+                FileName = _filePath,
+                IsHeightmap = true,
+            };
         }
-
         #endregion
         public MainWindowViewModel() 
         {
             OpenFileCommand = new RelayCommand(OpenPngFile);
-            GenerateCommand = new RelayCommand(AddHandler);
+            //GenerateCommand = new RelayCommand(AddHandler);
         }
     }
 }
